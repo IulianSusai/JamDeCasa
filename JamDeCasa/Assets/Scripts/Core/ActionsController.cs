0ac9b16bc96a9f57e275b2ff01be719b927e87f9
iulian <iuliansusai@gmail.com>
2020-05-01 00:13:50 +0300

    Project init & Ch Anims

---
+++ JamDeCasa/Assets/Scripts/Core/ActionsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionsController : MonoBehaviour
+{
+	
+	private static ActionsController instance;
+	public static ActionsController Instance {
+		get {
+			if(instance == null) {
+				instance = new ActionsController();
+			}
+			return instance;
+		}
+	}
+	private ActionsController() {}
+
+}
---
+++ JamDeCasa/Assets/Scripts/Player/Character.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+	[SerializeField] private float moveSpeed;
+	private CharacterController cc;
+
+	private void Awake() {
+		cc = GetComponent<CharacterController>();
+	}
+
+	private void Update() {
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+		Vector3 moveVector = new Vector3(vertical, 0f, -horizontal) * moveSpeed * Time.deltaTime; 
+		cc.Move(moveVector);
+	}
+
+}

8bc31592df11021a71b1e4f03001d7b1b7eef774
iulian <iuliansusai@gmail.com>
2020-05-01 23:56:11 +0300

    Obstacles & misc

---
+++ JamDeCasa/Assets/Scripts/Level/MoveToPointsObstacle.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToPointsObstacle : MonoBehaviour
+{
+
+	[SerializeField] private float moveToPointTime;
+	[SerializeField] private float moveToNextPointDelay;
+	[SerializeField] private AnimationCurve moveToPointCurve;
+	[SerializeField] private GameObject movingObject;
+	[SerializeField] private List<GameObject> points;
+
+	private bool isMoving;
+	private float moveStartTime;
+	private int currentPointIndex;
+
+	private Vector3 startPosition;
+	private Vector3 endPosition;
+
+	private void Start() {
+		currentPointIndex = 0;
+		movingObject.transform.position = points[0].transform.position;
+		movingObject.transform.LookAt(points[1].transform.position);
+	}
+
+	private void Awake() {
+		ActionsController.Instance.onLevelStart += OnLevelStarted;
+		ActionsController.Instance.onLevelDie += OnLevelDie;
+		ActionsController.Instance.onLevelWin += OnLevelWin;
+	}
+
+	private void OnDestroy() {
+		ActionsController.Instance.onLevelStart -= OnLevelStarted;
+		ActionsController.Instance.onLevelDie -= OnLevelDie;
+		ActionsController.Instance.onLevelWin -= OnLevelWin;
+	}
+
+	private void OnLevelStarted() {
+		StartMoving();
+	}
+
+	private void OnLevelDie() {
+		isMoving = false;
+		CancelInvoke();
+	}
+
+	private void OnLevelWin() {
+		isMoving = false;
+		CancelInvoke();
+	}
+
+
+	private void StartMoving() {
+		currentPointIndex++;
+		GameObject nextPoint = points[currentPointIndex % points.Count];
+		isMoving = true;
+		moveStartTime = Time.time;
+		startPosition = movingObject.transform.position;
+		endPosition = nextPoint.transform.position;
+		movingObject.transform.LookAt(nextPoint.transform);
+	}
+
+	private void Update() {
+		if (isMoving) {
+			UpdateMovement();
+		}
+	}
+
+
+	private void UpdateMovement() {
+		float timeSinceStart = Time.time - moveStartTime;
+		float percentage = timeSinceStart / moveToPointTime;
+		float curve = moveToPointCurve.Evaluate(percentage);
+		movingObject.transform.position = Vector3.Lerp(startPosition, endPosition, percentage);
+
+		if(percentage >= 1.0f) {
+			isMoving = false;
+			Invoke("StartMoving", moveToNextPointDelay);
+		}
+
+	}
+
+}
---
+++ JamDeCasa/Assets/Scripts/Level/ObstacleObject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleObject : MonoBehaviour
+{
+
+	private Animation anim;
+
+	private void Awake() {
+		anim = GetComponent<Animation>();
+		ActionsController.Instance.onLevelStart += OnLevelStart;
+		ActionsController.Instance.onLevelWin += OnLevelWin;
+		ActionsController.Instance.onLevelDie += OnLevelDie;
+	}
+
+	private void OnDestroy() {
+		ActionsController.Instance.onLevelStart -= OnLevelStart;
+		ActionsController.Instance.onLevelWin -= OnLevelWin;
+		ActionsController.Instance.onLevelDie -= OnLevelDie;
+	}
+
+	private void OnLevelStart() {
+		if(anim != null) {
+			anim[anim.clip.name].time = 0f;
+			anim.Play();
+		}
+	}
+
+	private void OnLevelWin() {
+		if(anim != null) {
+			anim.Stop();
+		}
+	}
+
+	private void OnLevelDie() {
+		if (anim != null) {
+			anim.Stop();
+		}
+	}
+}
